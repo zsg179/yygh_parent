a22b35fd8507aaa6d8571357cd1fdeb7676263ea
zsg179 <18977330642@163.com>
2021-05-21 17:28:32 +0800

    创建医院后台

---
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/ServiceHospApplication.java
@@ -0,0 +1,15 @@
+package com.atguigu.yygh.hosp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhusg02
+ * @date 2021/5/21 17:03
+ */
+@SpringBootApplication
+public class ServiceHospApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHospApplication.class, args);
+    }
+}
