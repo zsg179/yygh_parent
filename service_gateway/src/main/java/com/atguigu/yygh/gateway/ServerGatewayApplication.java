e3f283dc4cdad1d827a37bef55c28d78c123f8cc
zsg179 <18977330642@163.com>
2021-06-01 14:50:37 +0800

    增加gateway网关

---
+++ service_gateway/src/main/java/com/atguigu/yygh/gateway/ServerGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.atguigu.yygh.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhusg02
+ * @date 2021/6/1 9:30
+ */
+@SpringBootApplication
+public class ServerGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServerGatewayApplication.class, args);
+    }
+}
